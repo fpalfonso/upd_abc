ed3b2f18514ca5c5dc4c2df877f951339a4c16e1
fpalfonso <fpalfonso@up.edu.ph>
2024-03-25 01:36:50 +0800

    feat: added more test cases

--- erc20-staking/test/FDAToken.test.js
+++ erc20-staking/test/FDAToken.test.js
@@ -11,78 +11,86 @@ const {
     async function deploy() {
       const [account1, account2, account3] = await ethers.getSigners();
   
-      const MyToken = await ethers.getContractFactory("FDAToken");
-      const cMyToken = await MyToken.deploy(INITIAL_BALANCE, account1);
+      const FDAToken = await ethers.getContractFactory("FDAToken");
+      const cFDAToken = await FDAToken.deploy(INITIAL_BALANCE, account1);
   
-      return { cMyToken, account1, account2, account3 };
+      return { cFDAToken, account1, account2, account3 };
     }
   
     describe("Staking", function () {
       it("should call constructor", async function () {
-        const { cMyToken, account1 } = await loadFixture(deploy);
-        const balance = await cMyToken.balanceOf(account1.address);
+        const { cFDAToken, account1 } = await loadFixture(deploy);
+        const balance = await cFDAToken.balanceOf(account1.address);
         expect(balance).to.be.eq(INITIAL_BALANCE);
-        expect(cMyToken).not.to.be.undefined;
+        expect(cFDAToken).not.to.be.undefined;
       });
   
       it("should be able to mint", async function () {
-        const { cMyToken, account2 } = await loadFixture(deploy);
+        const { cFDAToken, account2 } = await loadFixture(deploy);
         const amount = ethers.parseEther("100");
-        await cMyToken["mint"](account2.address, amount);
-        const balance = await cMyToken.balanceOf(account2);
+        await cFDAToken["mint"](account2.address, amount);
+        const balance = await cFDAToken.balanceOf(account2);
         expect(balance).to.be.eq(amount);
       });
   
       it("should be able to stake", async function () {
-        const { cMyToken, account1 } = await loadFixture(deploy);
+        const { cFDAToken, account1 } = await loadFixture(deploy);
         const amount = ethers.parseEther("100");
-        // await cMyToken["mint"](account2.address, amount);
-        await cMyToken.connect(account1).stake(amount);
+        // await cFDAToken["mint"](account2.address, amount);
+        await cFDAToken.connect(account1).stake(amount);
   
-        const balance = await cMyToken.balanceOf(account1.address);
+        const balance = await cFDAToken.balanceOf(account1.address);
         expect(balance).to.be.eq(INITIAL_BALANCE - amount);
       });
   
       it("should be able to get current rewards", async function () {
-        const { cMyToken, account2 } = await loadFixture(deploy);
+        const { cFDAToken, account2 } = await loadFixture(deploy);
   
         const amount = ethers.parseEther("100");
-        await cMyToken["mint"](account2.address, amount);
+        await cFDAToken["mint"](account2.address, amount);
   
-        await cMyToken.connect(account2).stake(amount);
+        await cFDAToken.connect(account2).stake(amount);
   
         const ONE_HOUR_IN_SEC = 60 * 60;
         const unlockTime = (await time.latest()) + ONE_HOUR_IN_SEC;
   
         await time.increaseTo(unlockTime);
   
-        const rewards = await cMyToken.getCurrentRewards(account2.address);
+        const rewards = await cFDAToken.getCurrentRewards(account2.address);
   
         expect(rewards).to.be.greaterThan(ethers.parseEther("0"));
         //   console.log("current rewards", rewards);
       });
   
       it("should be able to claim rewards", async function () {
-        const { cMyToken, account2 } = await loadFixture(deploy);
+        const { cFDAToken, account2 } = await loadFixture(deploy);
   
         const amount = ethers.parseEther("100");
-        await cMyToken["mint"](account2.address, amount);
+        await cFDAToken["mint"](account2.address, amount);
   
-        await cMyToken.connect(account2).stake(amount);
+        await cFDAToken.connect(account2).stake(amount);
   
         const ONE_HOUR_IN_SEC = 60 * 60;
         const unlockTime = (await time.latest()) + ONE_HOUR_IN_SEC;
   
         await time.increaseTo(unlockTime);
   
-        await cMyToken.connect(account2).withdraw();
+        await cFDAToken.connect(account2).withdraw();
   
-        const balance = await cMyToken.balanceOf(account2);
+        const balance = await cFDAToken.balanceOf(account2);
         //   console.log("balance", balance);
       });
   
-      // ACTIVITY
-      xit("should not be able stake without token");
-      xit("should not be able to withdraw withouth a stake");
+      it("Should not allow withdrawing if no tokens are staked", async function () {
+        const { cFDAToken, account2 } = await loadFixture(deploy);
+        await expect(cFDAToken.connect(account2).withdraw()).to.be.revertedWith("No staked tokens");
+      });
+      
+      it("Should not allow staking more tokens than the balance", async function () {
+        const { cFDAToken, account2 } = await loadFixture(deploy);
+        await expect(cFDAToken.connect(account2).stake(2000000)).to.be.revertedWith(
+          "Insufficient balance"
+        );
+      });
     });
   });
